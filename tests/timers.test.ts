8c0851ef473637d03631452b465d95a84d7c8a7a
Jeff Martin <dist@dist.dev>
2022-06-17 20:16:35 +0100

    Added timer tests

---
+++ tests/timers.test.ts
@@ -0,0 +1,39 @@
+import 'ts-jest/utils';
+import { Timer } from '../src/timers';
+
+
+describe('Test Timers', () => {
+  const timer = new Timer('test')
+
+  it(`Timers: Should be initialized with an ID of test, and stopped`, () => {
+    expect(timer.id).toEqual('test')
+    expect(timer['state']).toEqual('Stopped')
+  })
+
+  it(`Timers: Should change state when started`, () => {
+    timer.setState('start')
+    expect(timer['state']).toEqual('Running')
+  })
+
+  it(`Timers: Should create times when requested`, () => {
+    expect(timer['time']).toHaveLength(0)
+    const now = (new Date()).getTime()
+    timer.setTime(0, now)
+    expect(timer['time'][0]).toEqual(now)
+  })
+
+  it(`Timers: Should adjust time when requested`, () => {
+    let previousTime = timer['start']
+    timer.setStart('00:00:00.000')
+    expect(timer['start']).toBeGreaterThan(previousTime)
+    const now = (new Date()).getTime()
+    timer.setTime(1, now)
+    expect(timer['time'][0]).toEqual(now)
+  })
+
+  it(`Timers: Should have 0 times and change state when reset`, () => {
+    timer.setState('reset')
+    expect(timer['state']).toEqual('Stopped')
+    expect(timer['time']).toHaveLength(0)
+  })
+});
